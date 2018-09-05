ad522350d36047315bac8e3c1bf40e0fa3ce1cae
Tony CALVEZ <tony.calvez@hotmail.fr>
2018-09-05 14:21:18 +0200

    Create COURS_3_EXO_1.java

---
+++ COURS_3_EXO_1.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class MyClass {
+    public static void main(String args[]){
+        double p = Math.PI * 3;
+        double c = Math.cos(p);
+        System.out.println(c);
+
+        Random r = new Random();
+        System.out.println(r.nextDouble());
+
+    }
+} 
