d37408f11477c110e384ae99917bd106dfd82e88
Tony Calvez <tony.calvez@hotmail.fr>
2020-01-22 13:03:56 +0100

    20200122-1300

--- INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/simulation/core/EngineInterruptReason.java
+++ INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/simulation/core/EngineInterruptReason.java
@@ -22,9 +22,7 @@ public enum EngineInterruptReason {
       /// <summary>
       /// Engine is interrupted because there are no more <see cref="Simulation.Core.SimTimeEvent">TimeEvent</see> to process
       /// nor any active <see cref="DirectSim.Simulation.Core.SimContinuous">Continous variable</see> 
-      /** The By empty. */
-      /// </summary>
-      ByEmpty,    // neither TimeEvent nor Continuous 
+    // neither TimeEvent nor Continuous
 
       /// <summary>
       /// Engine was interrupted by user using keyboard or mouse.
@@ -42,18 +40,13 @@ public enum EngineInterruptReason {
       /// Engine was interrupted because of the occurrence of a scheduled interruption.
       /** The By max date. */
      /// </summary>
-      ByMaxDate,     // occurrence of a scheduled interruption
+      ByMaxDate     // occurrence of a scheduled interruption
 
       /// <summary>
       /// Interruption was required by a SimObject (available for SimAgent only)
-      /** The By object. */
-     /// </summary>
-      ByObject,   // required by an SimObject 
+    // required by an SimObject
 
       /// <summary>
       /// Engine was stopped (pause) on user request or programmatically, by a Behavior Breakpoint
-      /** The By behavior. */
-   /// </summary>
-      ByBehavior // behavior breakpoint requiring user action
 }
 
