ba51a781a0d2213e04393aed6861a2720adba513
Tony Calvez <tony.calvez@hotmail.fr>
2020-01-22 13:01:46 +0100

    20200122-1300

--- INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/base/logger/loggerimpl/TypeAnalyseForLog.java
+++ INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/base/logger/loggerimpl/TypeAnalyseForLog.java
@@ -11,11 +11,8 @@ import java.util.HashMap;
  * The Class TypeAnalyseForLog.
  */
 public class TypeAnalyseForLog {
-	
-	/** The c. */
-	Class<?> c;
-	
-	/** The classement. */
+
+    /** The classement. */
 	String classement;
 	
 	/** The m L. */
@@ -32,8 +29,7 @@ public class TypeAnalyseForLog {
 	 * @param mL2 the m L 2
 	 */
 	public TypeAnalyseForLog(Class<? extends Object> class1, String name, HashMap<String, Method> mL2) {
-		c = class1;
-		classement = name;
+        classement = name;
 		if(mL2!=null) {			
 			titles = mL2.keySet().toArray(new String[0]);
 			mL=mL2;
--- INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/monitor/implementation/XYZRotator2.java
+++ INGENIERIE_SYSTEME/SIMULATION/src/enstabretagne/monitor/implementation/XYZRotator2.java
@@ -6,29 +6,8 @@ import javafx.scene.transform.Affine;
 import javafx.scene.transform.Rotate;
 
 public class XYZRotator2 {
-	public static void rotate(Node n,Point3D xyz) {
-		Affine a = new Affine();
-		
-		Rotate rz = new Rotate();
-		rz.setAxis(Rotate.Z_AXIS);
-		rz.setAngle(xyz.getZ());
-				
-		Rotate ry = new Rotate();
-		ry.setAxis(rz.transform(Rotate.Y_AXIS));
-//		ry.setAxis((Rotate.Y_AXIS));
-		ry.setAngle(xyz.getY());
 
-		a.prepend(rz);		
-		a.prepend(ry);
-		
-		System.out.println("z="+a.transform(Rotate.Z_AXIS));
-		System.out.println("y="+a.transform(Rotate.Y_AXIS));
-		
-		n.getTransforms().setAll(rz,ry);
-		
-	}
-	
-	public static Point3D getDirectionFromAngle(Point3D xyz) {
+    public static Point3D getDirectionFromAngle(Point3D xyz) {
 		Affine a = getTransformByAngle(xyz);
 		return a.transform(Rotate.X_AXIS);
 	}
